19f9618672e91daf2a40b32a77ab644e061e4ece
Garikoitz Bergara <garikoitz.bergara@jobandtalent.com>
2024-01-11 10:14:35 +0100

    Cleanup some old Postgres versions

--- postgres/hooks/vars.sh
+++ postgres/hooks/vars.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
 export VARS='
-  BASE_TAG=9.5-alpine
-  BASE_TAG=9.6-alpine
-  BASE_TAG=10.1-alpine
-  BASE_TAG=11.1-alpine
-  BASE_TAG=11.4-alpine
-  BASE_TAG=11.5-alpine
-  BASE_TAG=11.9-alpine
-  BASE_TAG=11.10-alpine
-  BASE_TAG=11.11-alpine
   BASE_TAG=11.16-alpine
   BASE_TAG=12.4-alpine
   BASE_TAG=12.5-alpine
